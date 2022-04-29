b90d0ace393822d1b6f64afbc99bfeef95064e45
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2022-04-30 09:17:03 +1000

    classic complete

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ import Header from "./components/Header/Header";
 
 //assets
 import logo from "../src/assets/logo.svg";
+import Link from "./components/Link/Link";
 
 function App() {
 
   return (
-    <div className="main" style={{ "--link": User.primaryLinkColor, "--text": User.primaryTextColor }}>
+    <div className="main">
       {/* TODO: change Data.js to a json file and replicate api call (does the same thing but is a little more realistic)  */}
       <Header userName={User.userName} userImage={User.profilePic} />
 
-      <div className="links">
-        <h1>Hello linktree</h1>
+      {/* Pass the styles through here so we do not have to add the colours to each component */}
+      <div className="links" style={{ "--link": User.primaryLinkColor, "--text": User.primaryTextColor }}>
+        {/* TODO: could map through these so no manual config needed on this page */}
+        <Link isClassic={true} title="Classic Link" User={User} link_url="https://github.com/mbrunkeECH/linktree" />
       </div>
+
 
       <div className="logo">
         {/* accessibility TODO: alt tags and general accessability*/}
